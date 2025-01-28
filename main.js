ea6dfbe1ea3db794acfe39f0acd232a9d131317e
alessiozaccato <alessiozaccato@gmail.com>
2025-01-28 16:23:04 +0100

    ADD:bonus section

--- main.js
+++ main.js
@@ -8,15 +8,35 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
 */
 
 let mailList = document.getElementById("list");
+let btnGenerate = document.getElementById("generate");
 
-for (i = 0; i < 9; i++) {
-    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.response)
-            mailList.innerHTML += ` <li class="list-group-item">${data.response}</li>`
-        })
+function emailGenerate() {
+    for (i = 0; i < 10; i++) {
+        fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.response)
+                mailList.innerHTML += ` <li class="list-group-item">${data.response}</li>`
+            })
+            .catch(error => {
+
+                console.error(error);
+
+            });
+
+    }
 }
+emailGenerate();
+
+
+btnGenerate.addEventListener("click", () => {
+
+    mailList.innerHTML = "";
+
+    emailGenerate();
+
+})
+
 
 
 
