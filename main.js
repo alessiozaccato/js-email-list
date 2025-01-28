d5f55894ae2b1810f2f0b70831f4e49eda59a33d
alessiozaccato <alessiozaccato@gmail.com>
2025-01-28 15:24:06 +0100

    ADD: solution of the main part of the exercise

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ Bonus
 Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 */
+
+let mailList = document.getElementById("list");
+
+for (i = 0; i < 9; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.response)
+            mailList.innerHTML += ` <li class="list-group-item">${data.response}</li>`
+        })
+}
+
+
+
+
+
+
+
